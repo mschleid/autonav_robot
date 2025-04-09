a0198175d03e0192526258b117092b24a4480903
Michael Schleider <schleidermike@gmail.com>
2025-04-08 20:23:05 -0400

    updated test script

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from autonav_uwb import UWBTag
+import time
 
 if __name__ == "__main__":
 
     tag = UWBTag(port='/dev/ttyTHS1', baudrate=115200, debug=True)
 
     def new_distance(*args, **kwargs):
-        
+
         print(f"new distance: {kwargs}")
 
     tag.on(tag.Event.DISTANCE, new_distance)
 
     try:
         tag.start()
+
+        while True:
+            time.sleep(0.1)
     except KeyboardInterrupt:
         tag.stop()
 
