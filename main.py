650752a167590fd7c4eab7f2537049626a8be281
Michael Schleider <schleidermike@gmail.com>
2025-04-14 18:18:44 -0400

    m

--- main.py
+++ main.py
@@ -96,8 +96,9 @@ def uwb_calculate_coordinates():
 def uwb_new_distance(*args, **kwargs):
     global tag_distances_from_anchors
     # print(f"new distance: {kwargs}")
-    print(kwargs)
-    # tag_distances_from_anchors[kwargs['address']] = kwargs['distance']
+    # print(kwargs)
+    tag_distances_from_anchors[kwargs['address']] = kwargs['distance']
+    print(tag_distances_from_anchors)
     # if len(tag_distances_from_anchors) >= 3:
     #     uwb_calculate_coordinates()
 
