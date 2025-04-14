3feb40ff13f84948a8b7be83b070a18c053bb2c8
Michael Schleider <schleidermike@gmail.com>
2025-04-14 18:19:42 -0400

    m

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ def uwb_new_distance(*args, **kwargs):
     # print(kwargs)
     tag_distances_from_anchors[kwargs['address']] = kwargs['distance']
     print(tag_distances_from_anchors)
-    # if len(tag_distances_from_anchors) >= 3:
-    #     uwb_calculate_coordinates()
+    if len(tag_distances_from_anchors) >= 3:
+        uwb_calculate_coordinates()
 
 def uwb_found_anchor(*args, **kwargs):
     print(f"found anchor: {args}")
